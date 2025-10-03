b4bf2a4cf17f3705d450be6e9ad4e931ba9a3455
spaceoden <26421479+spaceoden@users.noreply.github.com>
2025-10-03 14:35:23 -0700

    Delete files/scripts/mullvadbrowserstandardmalloc.sh

--- files/scripts/mullvadbrowserstandardmalloc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-sed -i 's|Exec=/usr/lib/mullvad-browser/start-mullvad-browser %u|Exec=env LD_PRELOAD= /usr/lib/mullvad-browser/start-mullvad-browser %u|' /usr/share/applications/mullvad-browser.desktop
